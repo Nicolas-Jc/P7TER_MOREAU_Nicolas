4ddaee9f96c5708b417a58891078ae3261cbbffc
A151UJ <A151UJ@login.axa>
2023-01-03 14:49:22 +0100

    Refact

--- src/main/java/com/openclassrooms/poseidon/controllers/LoginController.java
+++ src/main/java/com/openclassrooms/poseidon/controllers/LoginController.java
@@ -8,18 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("app")
+//@RequestMapping("app")
 public class LoginController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/login")
+    /*@GetMapping("/login")
     public ModelAndView login() {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("login");
         return mav;
-    }
+    }*/
 
     @GetMapping("secure/article-details")
     public ModelAndView getAllUserArticles() {
@@ -32,7 +32,7 @@ public class LoginController {
     @GetMapping("error")
     public ModelAndView error() {
         ModelAndView mav = new ModelAndView();
-        String errorMessage= "You are not authorized for the requested data.";
+        String errorMessage = "You are not authorized for the requested data.";
         mav.addObject("errorMsg", errorMessage);
         mav.setViewName("403");
         return mav;
